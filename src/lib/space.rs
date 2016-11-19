688ada224c9ba5a13c3a06f05f966cfb11293d5e
Matti Hänninen <matti@mjhanninen.com>
2016-11-19 19:22:37 +0200

    Introduce concept of frame, refactor spatial accessing

--- src/lib/space.rs
+++ src/lib/space.rs
@@ -110,10 +110,102 @@ impl Iterator for Neighbors
 }
 
 #[derive(Clone, Debug)]
+pub struct Frames<'a>
+{
+    space: &'a Space,
+    start: u16,
+    stop: u16,
+}
+
+impl<'a> Iterator for Frames<'a>
+{
+    type Item = Frame<'a>;
+
+    fn next(&mut self) -> Option<Self::Item>
+    {
+        if self.start != self.stop {
+            let f = Frame {
+                space: self.space,
+                origin: self.start,
+            };
+            self.start += 1;
+            Some(f)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Space
 {
+    sz: u16,
+    w: u16,
+    h: u16,
+    // These are going out of the door; don't increase their use.
     pub width: i16,
     pub height: i16,
+}
+
+#[derive(Clone, Debug)]
+pub struct Frame<'a>
+{
+    space: &'a Space,
+    origin: u16,
+}
+
+#[inline]
+fn modular_dist(a: u16, b: u16, m: u16) -> u16
+{
+    assert!(a < m && b < m);
+    let d1 = if a < b {
+        b - a
+    } else {
+        a - b
+    };
+    let d2 = m - d1;
+    if d1 < d2 {
+        d1
+    } else {
+        d2
+    }
+}
+
+impl<'a> Frame<'a>
+{
+    pub fn frames(&self) -> Frames
+    {
+        self.space.frames()
+    }
+
+    pub fn ix(&self) -> usize
+    {
+        self.origin as usize
+    }
+
+    pub fn coord(&self) -> Pos
+    {
+        let y = self.origin / self.space.w;
+        let x = self.origin - y * self.space.w;
+        Pos {
+            x: x as i16,
+            y: y as i16,
+        }
+    }
+
+    pub fn l1_norm(&self, other: &Frame) -> i16
+    {
+        assert!(self.space == other.space);
+        let w = self.space.w;
+        let h = self.space.h;
+        let y1 = self.origin / w;
+        let x1 = self.origin - y1 * w;
+        let y2 = other.origin / w;
+        let x2 = other.origin - y2 * w;
+        let dx = modular_dist(x1, x2, w);
+        let dy = modular_dist(y1, y2, h);
+        (dx + dy) as i16
+    }
 }
 
 #[inline]
@@ -148,6 +240,9 @@ impl Space
     {
         assert!(width > 0 && height > 0);
         Space {
+            sz: width as u16 * height as u16,
+            w: width as u16,
+            h: height as u16,
             width: width,
             height: height,
         }
@@ -206,6 +301,15 @@ impl Space
             } else {
                 Dir::South
             }
+        }
+    }
+
+    pub fn frames<'a>(&'a self) -> Frames<'a>
+    {
+        Frames {
+            space: &self,
+            start: 0,
+            stop: self.sz,
         }
     }
 }
