8ed6f5a2fa6848469e7e3ea417bc27381fe5f2ca
Matti Hänninen <matti@mjhanninen.com>
2016-11-29 09:06:41 +0200

    Reorganize space module
    By moving stuff around into hopefully more logical order and by grouping
    modular functions together into one module. Consider moving the modular
    functions under the math module.

--- src/lib/space.rs
+++ src/lib/space.rs
@@ -19,33 +19,6 @@ use coord::Coord;
 use dir::Dir;
 use map::Map;
 
-#[derive(Clone, Debug)]
-pub struct Frames<'a>
-{
-    space: &'a Space,
-    start: u16,
-    stop: u16,
-}
-
-impl<'a> Iterator for Frames<'a>
-{
-    type Item = Frame<'a>;
-
-    fn next(&mut self) -> Option<Self::Item>
-    {
-        if self.start != self.stop {
-            let f = Frame {
-                space: self.space,
-                origin: self.start,
-            };
-            self.start += 1;
-            Some(f)
-        } else {
-            None
-        }
-    }
-}
-
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Space
 {
@@ -54,28 +27,51 @@ pub struct Space
     h: u16,
 }
 
+impl Space
+{
+    pub fn with_dims(width: i16, height: i16) -> Self
+    {
+        assert!(width > 0 && height > 0);
+        Space {
+            sz: width as u16 * height as u16,
+            w: width as u16,
+            h: height as u16,
+        }
+    }
+
+    // Weird to call it "len" but, hey, that's conisitent with Rust's
+    // containers.
+    #[inline]
+    pub fn len(&self) -> usize
+    {
+        self.w as usize * self.h as usize
+    }
+
+    pub fn frames<'a>(&'a self) -> Frames<'a>
+    {
+        Frames {
+            space: &self,
+            start: 0,
+            stop: self.sz,
+        }
+    }
+
+    pub fn width(&self) -> i16
+    {
+        self.w as i16
+    }
+
+    pub fn height(&self) -> i16
+    {
+        self.h as i16
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct Frame<'a>
 {
     space: &'a Space,
     origin: u16,
-}
-
-#[inline]
-fn modular_dist(a: u16, b: u16, m: u16) -> u16
-{
-    assert!(a < m && b < m);
-    let d1 = if a < b {
-        b - a
-    } else {
-        a - b
-    };
-    let d2 = m - d1;
-    if d1 < d2 {
-        d1
-    } else {
-        d2
-    }
 }
 
 impl<'a> Frame<'a>
@@ -162,8 +158,8 @@ impl<'a> Frame<'a>
             L0_Neighbors {
                 space: self.space,
                 sz: 2 * r - 1,
-                x0: modular_sub(x, r - 1, self.space.w),
-                y0: modular_sub(y, r - 1, self.space.h),
+                x0: modular::sub(x, r - 1, self.space.w),
+                y0: modular::sub(y, r - 1, self.space.h),
                 dx: 0,
                 dy: 0,
             }
@@ -171,8 +167,8 @@ impl<'a> Frame<'a>
             L0_Neighbors {
                 space: self.space,
                 sz: 0,
-                x0: modular_sub(x, r - 1, self.space.w),
-                y0: modular_sub(y, r - 1, self.space.h),
+                x0: modular::sub(x, r - 1, self.space.w),
+                y0: modular::sub(y, r - 1, self.space.h),
                 dx: 0,
                 dy: 0,
             }
@@ -189,9 +185,36 @@ impl<'a> Frame<'a>
         let x1 = self.origin - y1 * w;
         let y2 = other.origin / w;
         let x2 = other.origin - y2 * w;
-        let dx = modular_dist(x1, x2, w);
-        let dy = modular_dist(y1, y2, h);
+        let dx = modular::dist(x1, x2, w);
+        let dy = modular::dist(y1, y2, h);
         (dx + dy) as i16
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Frames<'a>
+{
+    space: &'a Space,
+    start: u16,
+    stop: u16,
+}
+
+impl<'a> Iterator for Frames<'a>
+{
+    type Item = Frame<'a>;
+
+    fn next(&mut self) -> Option<Self::Item>
+    {
+        if self.start != self.stop {
+            let f = Frame {
+                space: self.space,
+                origin: self.start,
+            };
+            self.start += 1;
+            Some(f)
+        } else {
+            None
+        }
     }
 }
 
@@ -206,29 +229,6 @@ pub struct L0_Neighbors<'a>
     dy: u16,
 }
 
-#[inline]
-fn modular_add(a: u16, b: u16, m: u16) -> u16
-{
-    assert!(a < m && b < m);
-    let c = a + b;
-    if c < m {
-        c
-    } else {
-        c - m
-    }
-}
-
-#[inline]
-fn modular_sub(a: u16, b: u16, m: u16) -> u16
-{
-    assert!(a < m && b < m);
-    if a >= b {
-        a - b
-    } else {
-        a + m - b
-    }
-}
-
 impl<'a> Iterator for L0_Neighbors<'a>
 {
     type Item = Frame<'a>;
@@ -240,8 +240,8 @@ impl<'a> Iterator for L0_Neighbors<'a>
         } else if self.dy == self.sz {
             None
         } else {
-            let x = modular_add(self.x0, self.dx, self.space.w);
-            let y = modular_add(self.y0, self.dy, self.space.h);
+            let x = modular::add(self.x0, self.dx, self.space.w);
+            let y = modular::add(self.y0, self.dy, self.space.h);
             self.dx += 1;
             if self.dx == self.sz {
                 self.dx = 0;
@@ -256,60 +256,63 @@ impl<'a> Iterator for L0_Neighbors<'a>
     }
 }
 
-impl Space
-{
-    pub fn with_dims(width: i16, height: i16) -> Self
-    {
-        assert!(width > 0 && height > 0);
-        Space {
-            sz: width as u16 * height as u16,
-            w: width as u16,
-            h: height as u16,
-        }
-    }
+mod modular {
 
-    // Weird to call it "len" but, hey, that's conisitent with Rust's
-    // containers.
     #[inline]
-    pub fn len(&self) -> usize
+    pub fn add(a: u16, b: u16, m: u16) -> u16
     {
-        self.w as usize * self.h as usize
-    }
-
-    pub fn frames<'a>(&'a self) -> Frames<'a>
-    {
-        Frames {
-            space: &self,
-            start: 0,
-            stop: self.sz,
+        assert!(a < m && b < m);
+        let c = a + b;
+        if c < m {
+            c
+        } else {
+            c - m
         }
     }
 
-    pub fn width(&self) -> i16
+    #[inline]
+    pub fn sub(a: u16, b: u16, m: u16) -> u16
     {
-        self.w as i16
+        assert!(a < m && b < m);
+        if a >= b {
+            a - b
+        } else {
+            a + m - b
+        }
     }
 
-    pub fn height(&self) -> i16
+    #[inline]
+    pub fn dist(a: u16, b: u16, m: u16) -> u16
     {
-        self.h as i16
+        assert!(a < m && b < m);
+        let d1 = if a < b {
+            b - a
+        } else {
+            a - b
+        };
+        let d2 = m - d1;
+        if d1 < d2 {
+            d1
+        } else {
+            d2
+        }
     }
-}
 
-#[cfg(test)]
-mod test {
+    #[cfg(test)]
+    mod test {
 
-    #[test]
-    fn test_modular_sub()
-    {
-        for m in 1..100 {
-            for a in 0..m {
-                for b in 0..m {
-                    let c = super::modular_sub(a, b, m);
-                    if c >= m {
-                        panic!("failed for a={}, b={}, m={}", a, b, m);
+        #[test]
+        fn test_sub()
+        {
+            for m in 1..100 {
+                for a in 0..m {
+                    for b in 0..m {
+                        let c = super::sub(a, b, m);
+                        if c >= m {
+                            panic!("failed for a={}, b={}, m={}", a, b, m);
+                        }
+                        assert_eq!((c + b) % m, a);
                     }
-                    assert_eq!((c + b) % m, a);
                 }
             }
         }
